29f5e76f1ed063f2dc3c27f1cc9ac382ce84de21
chSaurav <48649257+chSaurav@users.noreply.github.com>
2020-01-03 00:20:01 +0530

    Create hostogram.py

---
+++ hostogram.py
@@ -0,0 +1,9 @@
+file = open("histogram.txt")
+for line in file:
+    # print( line)
+    l = line.split(" ")
+    prn = 1[0]
+    mrk = 1[1]
+    print(1, end="")
+    print((int(1[1]) - 75) * "#")
+
