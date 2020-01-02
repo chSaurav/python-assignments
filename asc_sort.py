0f0ece4a3f26000290be5a15b3274adec2bc7443
chSaurav <48649257+chSaurav@users.noreply.github.com>
2020-01-02 23:53:23 +0530

    Update asc_sort.py

--- asc_sort.py
+++ asc_sort.py
@@ -1,18 +1,22 @@
 #function
-def ascsort(arr):
-	for i in range(0,len(arr)):
-		for j in range(i+1,len(arr)):
-			if(arr[i] > arr[j]):
-				temp = arr[i]
-				arr[i] = arr[j]
-				inum[j] = temp
-	#displaying elements in ascending order
-	for i in range(len(arr)):
-		print(arr[i])
+# Python Program to Sort List in Ascending Order
 
-#driverCode
-arr = []
-for i in range(0,10):
-	num = int(input("Enter No : "))
-	arr.append(num)
-ascsort(arr)
+NumList = []
+
+Number = int(input("Please enter the Total Number of List Elements: "))
+for i in range(1, Number + 1):
+    value = int(input("Please enter the Value of %d Element : " %i))
+    NumList.append(value)
+
+i = 0
+while(i < Number):
+    j = i + 1
+    while(j < Number):
+        if(NumList[i] > NumList[j]):
+            temp = NumList[i]
+            NumList[i] = NumList[j]
+            NumList[j] = temp
+        j = j + 1
+    i = i + 1
+
+print("Element After Sorting List in Ascending Order is : ", NumList)
