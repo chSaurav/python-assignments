6274d3c7c9320ba76ec1e569c67d49f68057df3d
chSaurav <48649257+chSaurav@users.noreply.github.com>
2020-01-03 00:12:27 +0530

    Create star pattern 1.py

---
+++ star pattern 1.py
@@ -0,0 +1,14 @@
+MAX_VALUE = 3
+
+def printTop():
+    for i in range (MAX_VALUE):
+        for j in range (i):
+            print (j)
+        print
+printTop()
+def printTop():
+    for i in range (MAX_VALUE,0,-1):
+        for j in range (i,0,-1):
+            print (MAX_VALUE)
+        print
+printTop()
